fe190a0bc90396b8cb0e9f9554a192660bb8ea77
AIME JOSEPH ANDRIANOMENTSOA <j.andria@ergosante.fr>
2023-12-22 15:41:52 +0300

    update base

--- server/src/datasources/main.datasource.ts
+++ server/src/datasources/main.datasource.ts
@@ -1,7 +1,7 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
-const config = {
+/*const config = {
   name: 'main',
   connector: 'mysql',
   url: '',
@@ -10,27 +10,29 @@ const config = {
   user: 'root',
   password: '',
   database: 'ergomada',
-};
+};*/
 
 // simafri
-/*const config = {
+const config = {
   name: 'main',
   connector: 'mysql',
   url: '',
   host: '178.159.5.244',
   //port: 3306,
-  user: 'ajadimg_ergo',
+  user: 'ajadimg_ergomada',
   password: 'ergo',
-  database: 'ajadimg_ergo',
-};*/
+  database: 'ajadimg_ergomada',
+};
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class MainDataSource extends juggler.DataSource
-  implements LifeCycleObserver {
+export class MainDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
   static dataSourceName = 'main';
   static readonly defaultConfig = config;
 
