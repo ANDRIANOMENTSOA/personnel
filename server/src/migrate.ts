978a881714ae7f53bc65605088c476dbb9fa6f5d
Aimé <aaimjoseph@gmail.com>
2026-02-17 11:58:14 +0300

    update migrate

--- server/src/migrate.ts
+++ server/src/migrate.ts
@@ -1,5 +1,9 @@
 import {ApiApplication} from './application';
-import { PersonnelRepository } from './repositories';
+import {
+  PersonnelCredentialRepository,
+  PersonnelRepository,
+} from './repositories';
+import bcrypt from 'bcrypt';
 
 export async function migrate(args: string[]) {
   const existingSchema = args.includes('--rebuild') ? 'drop' : 'alter';
@@ -8,6 +12,28 @@ export async function migrate(args: string[]) {
   const app = new ApiApplication();
   await app.boot();
   await app.migrateSchema({existingSchema});
+
+  /*
+  const perso = await app.getRepository(PersonnelRepository);
+
+  const personnel = await perso.create({
+    nom: 'ANDRIANOMENTSOA',
+    prenom: 'Aimé Joseph',
+    email: 'aaimjoseph@gmail.com',
+    description_mission: 'description_mission',
+  });
+
+  const salt = await bcrypt.genSalt(6);
+
+  const credentials = {
+    hash: await bcrypt.hash('Ajadinyaiko010028.', salt),
+    personnelId: personnel.id,
+  };
+
+  const crendential = await app.getRepository(PersonnelCredentialRepository);
+
+  await crendential.create(credentials);
+  */
 
 
   // Connectors usually keep a pool of opened connections,
